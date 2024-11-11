de0befabdad18030d69570edb2ffe38a08cc5c6d
Indiara C. Menegat <indicm@gmail.com>
2024-11-11 01:27:34 -0300

    LAMBDA HANDLER

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,46 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {
+  checkPath,
+  httpErrorToLambdaResponse,
+  httpResponseToLambdaResponse,
+  parseStringToJson,
+} from 'common/Utils';
+import { CategorizeBody, CreateProfileDataBody } from 'contracts/server/Http';
+import {
+  Categorization,
+  ProfileAnalysisGeneration,
+  ProfileDataGeneration,
+} from './factory';
 
 export const handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  const { path, pathParameters, body, httpMethod, queryStringParameters } =
+    event;
+
   console.log('Received event:', JSON.stringify(event, null, 2));
+  console.info(`[${new Date().toISOString()}] ${httpMethod} ${path}`);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from Lambda!',
-      input: event,
-    }),
-  };
-
-  return response;
+  try {
+    if (httpMethod === 'POST' && checkPath(path, 'categorize')) {
+      const parsedBody = parseStringToJson<CategorizeBody>(body);
+      const result = await Categorization.handle({ body: parsedBody });
+      return httpResponseToLambdaResponse(result);
+    } else if (httpMethod === 'POST' && checkPath(path, 'data')) {
+      const parsedBody = parseStringToJson<CreateProfileDataBody>(body);
+      const result = await ProfileDataGeneration.handle({ body: parsedBody });
+      return httpResponseToLambdaResponse(result);
+    } else if (httpMethod === 'GET' && checkPath(path, 'analysis')) {
+      const { username } = pathParameters || {};
+      const { force } = queryStringParameters || {};
+      const result = await ProfileAnalysisGeneration.handle({
+        params: { username },
+        query: { force },
+      });
+      return httpResponseToLambdaResponse(result);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return httpErrorToLambdaResponse(error);
+  }
 };
