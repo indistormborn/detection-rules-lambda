93c66443d373bbdd6ad45137c19b88374573f179
Indiara C. Menegat <indicm@gmail.com>
2024-11-11 02:32:33 -0300

    tiny tiny fixes

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Buildar o projeto TypeScript
-npx tsc
+npm run build:prod
 
 # Instalar dependências de produção
-mkdir -p dist/node_modules
-cp package.json package-lock.json dist/
-cd dist
+mkdir -p dist/prod/node_modules
+cp package.json package-lock.json dist/prod/
+cd dist/prod
 npm install --production
 cd ..
 
 # Zipar os arquivos
-cd dist
-zip -r ../lambda-function.zip *
-cd ..
+zip -r lambda-function.zip prod/*
 
 # Fazer o upload para o AWS Lambda
 aws lambda update-function-code --function-name detection-rules --zip-file fileb://lambda-function.zip --profile lambda-deploy
+
+# Limpar o arquivo zip após o upload
+rm lambda-function.zip
